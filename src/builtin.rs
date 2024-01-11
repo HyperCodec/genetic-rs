959a2b9c6586f7fb2b8b809cebc11b95decc10f5
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2024-01-11 18:03:55 +0000

    create base api

---
+++ src/builtin.rs
@@ -0,0 +1,73 @@
+pub trait ASexualEntity {
+    /// Mutate the entity with a given mutation rate (0..1)
+    fn mutate(&mut self, rate: f32);
+
+    /// Create a new child with mutation. Similar to [mutate][ASexualEntity::mutate], but returns a new instance instead of modifying the original.
+    /// If it is simply returning a cloned and mutated version, consider using a constant default mutation rate.
+    fn spawn_child(&self) -> Self;
+}
+
+// TODO sexual
+
+pub trait Prunable {
+    /// This does any unfinished work in the despawning process.
+    /// It doesn't need to be implemented unless in specific usecases where your algorithm needs to explicitly despawn an entity.
+    fn despawn(&mut self) {}
+}
+
+pub mod next_gen {
+    use super::*;
+
+    /// When making a new generation, it mutates each entity a certain amount depending on their reward.
+    pub fn asexual_scrambling_nextgen<E: ASexualEntity>(mut rewards: Vec<(E, f32)>) -> Vec<E> {
+        rewards.sort_by(|(_, r1), (_, r2)| r1.partial_cmp(r2).unwrap());
+
+        let len = rewards.len() as f32;
+
+        rewards
+            .into_iter()
+            .enumerate()
+            .map(|(i, (mut e, _))| {
+                e.mutate(i as f32 / len);
+                e
+            })
+            .collect()
+    }
+
+    /// When making a new generation, it despawns half of the entities and then spawns children from the remaining to reproduce.
+    /// WIP: const generic for mutation rate, will allow for [ASexualEntity::spawn_child] to accept a custom mutation rate. Delayed due to current Rust limitations
+    pub fn asexual_pruning_nextgen<E: ASexualEntity + Prunable + Clone>(rewards: Vec<(E, f32)>) -> Vec<E> {
+        let sum: f32 = rewards
+            .iter()
+            .map(|(_, r)| r)
+            .sum();
+
+        let population_size = rewards.len();
+        let avg = sum / population_size as f32;
+
+        let mut next_gen: Vec<E> = rewards
+            .into_iter()
+            .filter_map(|(mut e, r)| {
+                if r < avg {
+                    e.despawn();
+                    return None;
+                }
+
+                Some(e)
+            })
+            .collect();
+
+        let mut og_champions = next_gen
+            .clone() // TODO remove if possible. currently doing so because `next_gen` is borrowed as mutable later
+            .into_iter()
+            .cycle();
+        
+        while next_gen.len() < population_size {
+            let e = og_champions.next().unwrap();
+
+            next_gen.push(e.spawn_child());
+        }
+
+        next_gen
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,48 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use replace_with::replace_with_or_abort;
+
+#[cfg(feature = "builtin")]
+pub mod builtin;
+
+pub struct GeneticSim<E>
+where
+    E: Sized,
+{
+    entities: Vec<E>, // should work without actually owning the entities
+    reward: Box<dyn Fn(&E) -> f32>,
+    next_gen: Box<dyn Fn(Vec<(E, f32)>) -> Vec<E>>,
+}
+
+impl<E> GeneticSim<E>
+where
+    E: Sized,
+{
+    pub fn new(
+        starting_entities: Vec<E>,
+        reward: impl Fn(&E) -> f32 + 'static, 
+        next_gen: impl Fn(Vec<(E, f32) >) -> Vec<E> + 'static
+    ) -> Self {
+        Self {
+            entities: starting_entities,
+            reward: Box::new(reward),
+            next_gen: Box::new(next_gen),
+        }
+    }
+
+    /// Uses the `next_gen` provided in [GeneticSim::new] to create the next generation of 
+    pub fn next_generation(&mut self) {
+        // TODO maybe remove unneccessary dependency, can prob use std::mem::replace
+        replace_with_or_abort(&mut self.entities, |entities| {
+            let rewards = entities
+                .into_iter()
+                .map(|e| {
+                    let reward = (self.reward)(&e);
+                    (e, reward)
+                })
+                .collect();
+
+            (self.next_gen)(rewards)
+        });
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +50,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_api() {
+        
     }
 }
