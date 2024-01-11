b49d286ec34c178486145415daa70e5c85f11c82
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2024-01-11 19:27:47 +0000

    add asex unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@ pub struct GeneticSim<E>
 where
     E: Sized,
 {
-    entities: Vec<E>, // should work without actually owning the entities
+    /// The current population of entities
+    pub entities: Vec<E>,
     reward: Box<dyn Fn(&E) -> f32>,
     next_gen: Box<dyn Fn(Vec<(E, f32)>) -> Vec<E>>,
 }
@@ -61,10 +62,65 @@ where
 
 #[cfg(test)]
 mod tests {
+
     use super::prelude::*;
+    use rand::prelude::*;
+
+    #[derive(Default, Clone, Debug)]
+    struct MyEntity(f32);
+
+    impl ASexualEntity for MyEntity {
+        fn mutate(&mut self, rate: f32) {
+            let mut rng = rand::thread_rng();
+            self.0 += rng.gen::<f32>() * rate;
+        }
+
+        fn spawn_child(&self) -> Self {
+            let mut child = self.clone();
+            child.mutate(0.25);
+            child
+        }
+    }
+
+    impl Prunable for MyEntity {
+        fn despawn(&mut self) {
+            println!("RIP {:?}", self);
+        }
+    }
+
+    const MAGIC_NUMBER: f32 = std::f32::consts::E;
+
+    fn my_reward_fn(ent: &MyEntity) -> f32 {
+        (MAGIC_NUMBER - ent.0).abs() * -1.
+    }
 
     #[test]
-    fn test_api() {
-        
+    fn as_scramble() {
+        let mut sim = GeneticSim::new(
+            vec![MyEntity::default(); 100], 
+            my_reward_fn, 
+            asexual_scrambling_nextgen,
+        );
+
+        for _ in 0..100 {
+            sim.next_generation();
+        }
+
+        dbg!(sim.entities);
+    }
+
+    #[test]
+    fn as_prune() {
+        let mut sim = GeneticSim::new(
+            vec![MyEntity::default(); 100],
+            my_reward_fn,
+            asexual_pruning_nextgen,
+        );
+
+        for _ in 0..100 {
+            sim.next_generation();
+        }
+
+        dbg!(sim.entities);
     }
 }
