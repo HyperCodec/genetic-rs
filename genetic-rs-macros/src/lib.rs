7a5036ae452756eee4d24d027964cbffb1cf46db
Tristan Murphy <72839119+inflectrix@users.noreply.github.com>
2024-02-26 13:04:28 +0000

    add randmut derive

--- genetic-rs-macros/src/lib.rs
+++ genetic-rs-macros/src/lib.rs
@@ -1,2 +1,33 @@
-use genetic_rs_common::prelude::*;
+extern crate proc_macro;
 
+use genetic_rs_common::prelude::*;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
+
+#[proc_macro_derive(RandomlyMutable)]
+pub fn randmut_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    let mut inner_mutate = quote!();
+
+    if let Data::Struct(data) = ast.data {
+        match &data.fields {
+            Fields::Named(named) => for field in named.named.iter() {
+                let name = field.ident.clone().unwrap();
+                inner_mutate.extend(quote!(self.#name.mutate(rate, rng);));
+            }
+            _ => unimplemented!(),
+        }
+    
+    }
+
+    let name = &ast.ident;
+    quote! {
+        impl RandomlyMutable for #name {
+            fn mutate(&mut self, rate: f32, rng: &mut impl rand::Rng) {
+                #inner_mutate
+            }
+        }
+    }.into()
+}
