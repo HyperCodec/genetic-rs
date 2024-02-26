b75b6f009854820217e5101dba68dfb66c3a2682
Tristan Murphy <72839119+inflectrix@users.noreply.github.com>
2024-02-26 15:36:01 +0000

    add prunable derive

--- genetic-rs-macros/src/lib.rs
+++ genetic-rs-macros/src/lib.rs
@@ -36,13 +36,13 @@ pub fn randmut_derive(input: TokenStream) -> TokenStream {
 pub fn divrepr_derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     
-    let mut inner_divide = quote!();
+    let mut inner_divide_return = quote!();
 
     if let Data::Struct(data) = ast.data {
         match &data.fields {
             Fields::Named(named) => for field in named.named.iter() {
                 let name = field.ident.clone().unwrap();
-                inner_divide.extend(quote!(self.#name.divide(rng);));
+                inner_divide_return.extend(quote!(#name: self.#name.divide(rng),));
             },
             _ => unimplemented!()
         }
@@ -53,7 +53,36 @@ pub fn divrepr_derive(input: TokenStream) -> TokenStream {
     quote! {
         impl DivisionReproduction for #name {
             fn divide(&self, &mut impl rand::Rng) -> Self {
-                #inner_divide
+                Self {
+                    #inner_divide_return
+                }
+            }
+        }
+    }.into()
+}
+
+#[proc_macro_derive(Prunable)]
+pub fn prunable_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+
+    let mut inner_despawn = quote!();
+
+    if let Data::Struct(data) = ast.data {
+        match &data.fields {
+            Fields::Named(named) => for field in named.named.iter() {
+                let name = field.ident.clone().unwrap();
+                inner_despawn.extend(quote!(self.#name.despawn();));
+            },
+            _ => unimplemented!()
+        }
+    }
+
+    let name = &ast.ident;
+
+    quote! {
+        impl Prunable for #name {
+            fn despawn(self) {
+                #inner_despawn
             }
         }
     }.into()
