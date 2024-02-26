8067aa0a2b4ee3b77ccfbf8a461a3811dbb56d2f
Tristan Murphy <72839119+inflectrix@users.noreply.github.com>
2024-02-26 14:22:54 +0000

    add division reprod derive

--- genetic-rs-macros/src/lib.rs
+++ genetic-rs-macros/src/lib.rs
@@ -31,3 +31,30 @@ pub fn randmut_derive(input: TokenStream) -> TokenStream {
         }
     }.into()
 }
+
+#[proc_macro_derive(DivisionReproduction)]
+pub fn divrepr_derive(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as DeriveInput);
+    
+    let mut inner_divide = quote!();
+
+    if let Data::Struct(data) = ast.data {
+        match &data.fields {
+            Fields::Named(named) => for field in named.named.iter() {
+                let name = field.ident.clone().unwrap();
+                inner_divide.extend(quote!(self.#name.divide(rng);));
+            },
+            _ => unimplemented!()
+        }
+    }
+
+    let name = &ast.ident;
+
+    quote! {
+        impl DivisionReproduction for #name {
+            fn divide(&self, &mut impl rand::Rng) -> Self {
+                #inner_divide
+            }
+        }
+    }.into()
+}
